5176b62abce54825e5fa22acce7e333d179ef4f8
CMIHGH <cmihghanadata@gmail.com>
2024-06-24 16:40:48 +0000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
 // firebase-config.js
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCS6Ov5waek8isRQ_rkSc-Fn-q7RGJfiEk",
+  authDomain: "dashboard-a88f5.firebaseapp.com",
+  projectId: "dashboard-a88f5",
+  storageBucket: "dashboard-a88f5.appspot.com",
+  messagingSenderId: "560573560325",
+  appId: "1:560573560325:web:1c991babe9110921f2ce6c",
+  measurementId: "G-ZP65HDHB15"
 };
 firebase.initializeApp(firebaseConfig);
